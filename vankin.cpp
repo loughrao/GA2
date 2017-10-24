1e356ab27871b6227c45fd1e1acaf1d50a62bad2
Owen Loughran <loughrao@flip1.engr.oregonstate.edu>
2017-10-23 19:04:22 -0700

    Speeeeeed

--- vankin.cpp
+++ vankin.cpp
@@ -5,6 +5,26 @@
 
 using namespace std;
 
+int Comp(int a, int b) {
+  if (a > b) {
+    return a;
+  } else {
+    return b;
+  }
+}
+
+int Max(vector<vector<int>> Arr, vector<vector<int>> Soln) {
+  int n = Arr.size();
+  int Max = -101;
+  for (int i = n-1; i >= 0; i--) {
+    for (int j = n-1; j >= 0; j--) {
+      Soln[i][j] = Arr[i][j] + Comp(Soln[i+1][j],Soln[i][j+1]);
+      Max = Comp(Soln[i][j],Max);
+    }
+  }
+  return Max;
+}
+
 int getRows(ifstream &inFile){
   string temp;
   getline(inFile,temp);
@@ -13,9 +33,10 @@ int getRows(ifstream &inFile){
 
 int main(){
   ifstream inFile("input.txt", ios::in);
-  if(!inFile)
-    return 0;
-
+  if(!inFile) {
+    cout << "No Output file" << endl;
+    return 0; 
+  }
   int n = getRows(inFile);
 
   string temp;
@@ -26,14 +47,20 @@ int main(){
         getline(inFile,temp);
       else
         getline(inFile,temp,',');
-      cout << "["<<i<<"]["<<j<<"]: " << temp << endl;
-      array[i][j] = stoi(temp);
-      
+      array[i][j] = stoi(temp);  
     }
   }
 
-  cout << "vector pos 1,1: " << array[1][1] << endl;
-
+  vector<vector<int>> solution(n+1, vector<int>(n+1));
+  for(int i= 0; i <= n; i++) {
+    for(int j = 0; j <= n; j++) {
+      solution[i][j] = 0;
+    }
+  }
+  
+  int max;
+  max = Max(array, solution);
+  cout << max << endl;
   inFile.close();
   return 0;
 }
